18ff98b6d6c1856a5855037c7e3ae07bebc50fe9
evrob0095 <evrob0095@7L9N6Y1.Student.UGDSB.ED>
2020-02-24 13:58:00 -0500

    First Commit

---
+++ src/palindrome3/Palindrome3.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package palindrome3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author evrob0095
+ */
+public class Palindrome3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String palindrome = JOptionPane.showInputDialog("Enter a word to check");
+        char[] arr = new char[palindrome.length()];
+        for(int i = 0; i < palindrome.length(); i++){
+            if (palindrome.charAt(i) == ' ') {
+                System.out.println("Hi");
+            } else {
+                arr[i] = palindrome.charAt(i);
+                
+            }
+            
+        }
+        System.out.println(arr);
+        String newString = "";
+        String oldString = "";
+        for(int i = 0; i < palindrome.length(); i++){
+            newString = arr[i] + newString;
+            System.out.println(newString);
+            oldString = oldString + arr[i];
+            System.out.println(oldString);
+        }
+        if (oldString.equals(newString)) {
+            JOptionPane.showMessageDialog(null, "Your word: " + palindrome + ", "
+                    + "is the same when reversed and is therefore a palindrome");
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Your word: " + palindrome + ", "
+                    + "is not the same when reversed and is therefore is not a palindrome");
+        }
+    }
+    
+}
